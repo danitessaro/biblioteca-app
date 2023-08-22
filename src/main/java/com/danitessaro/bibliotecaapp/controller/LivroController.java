fca103ef18532d4e4c3adec499fd88b609be4948
danitessaro <danitcain@gmail.com>
2023-08-22 18:52:28 -0300

    Adicionando Controllers

---
+++ src/main/java/com/danitessaro/bibliotecaapp/controller/LivroController.java
@@ -0,0 +1,77 @@
+package com.danitessaro.bibliotecaapp.controller;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.danitessaro.bibliotecaapp.model.Livro;
+import com.danitessaro.bibliotecaapp.model.Usuario;
+import com.danitessaro.bibliotecaapp.service.LivroService;
+
+@RequestMapping("/livro")
+@RestController
+public class LivroController {
+
+    private final LivroService livroService;
+
+    public LivroController (LivroService livroService) {
+        this.livroService = livroService;
+    }
+    
+    @PostMapping
+    public ResponseEntity createLivro(@RequestBody Livro livro) throws URISyntaxException {
+        Livro livroAdicionado = livroService.adicionarLivro(livro);
+        return ResponseEntity.created(new URI("/livros/" + livroAdicionado.getId())).body(livroAdicionado);
+    }
+
+    @GetMapping("/{id}")
+    public Livro buscarLivro(@PathVariable Long id) {
+        return livroService.buscarLivro(id);
+    }
+
+    @GetMapping("/autor")
+    public List<Livro> buscarLivros(@RequestParam String autor) {
+        return livroService.buscarLivros(autor);
+    }
+
+    @GetMapping("/titulo")
+    public Livro buscarLivro(@RequestParam String titulo) {
+        return livroService.buscarLivro(titulo);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity updateLivro(@PathVariable Long id, @RequestBody Livro novoLivro) {
+        Livro livroAtualizado = livroService.editarLivro(id, novoLivro);
+        return ResponseEntity.ok(livroAtualizado);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity excluirLivro(@PathVariable Long id) {
+        livroService.excluirLivro(id);
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/{id}/emprestimo")
+    public ResponseEntity emprestarLivro(@PathVariable Long id, @RequestBody Usuario usuario) throws Exception {
+        Livro livro = livroService.emprestarLivro(id, usuario.getId());
+        return ResponseEntity.ok(livro);
+
+    }
+
+    @PostMapping("/{id}/devolucao")
+    public ResponseEntity devolverLivro(@PathVariable Long id) {
+        Livro livro = livroService.devolverLivro(id);
+        return ResponseEntity.ok(livro);
+    }
+}
---
+++ src/main/java/com/danitessaro/bibliotecaapp/controller/UsuarioController.java
@@ -0,0 +1,58 @@
+package com.danitessaro.bibliotecaapp.controller;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.danitessaro.bibliotecaapp.model.Usuario;
+import com.danitessaro.bibliotecaapp.service.UsuarioService;
+
+@RestController
+@RequestMapping("/usuarios")
+public class UsuarioController {
+    
+    private final UsuarioService usuarioService;
+
+    public UsuarioController(UsuarioService usuarioService) {
+        this.usuarioService = usuarioService;
+    }
+
+    @PostMapping
+    public ResponseEntity createUsuario(@RequestBody Usuario usuario) throws URISyntaxException {
+        Usuario usuarioAdicionado = usuarioService.adicionarUsuario(usuario);
+        return ResponseEntity.created(new URI("/usuarios" + usuarioAdicionado.getId())).body(usuarioAdicionado);
+    }
+
+   @GetMapping("/{id}")
+   public Usuario getUsuario(@PathVariable Long id) {
+       return usuarioService.buscarUsuario(id);
+   }
+
+   @GetMapping
+   public Usuario getUsuario(@RequestParam String nome) {
+       return usuarioService.buscarUsuario(nome);
+   }
+
+   @PutMapping("/{id}")
+    public ResponseEntity updateUsuario(@PathVariable Long id, @RequestBody Usuario usuario) {
+        Usuario usuarioAtualizado = usuarioService.editarUsuario(id, usuario);
+        return ResponseEntity.ok(usuarioAtualizado);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity excluirUsuario(@PathVariable Long id) {
+        usuarioService.excluirUsuario(id);
+        return ResponseEntity.ok().build();
+    }
+}
